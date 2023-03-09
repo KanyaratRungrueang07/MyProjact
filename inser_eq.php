a92736307a81ea51fab54d6b7cc2aa8d1831eb90
KanyaratRungrueang07 <86422790+KanyaratRungrueang07@users.noreply.github.com>
2023-03-09 22:04:48 +0700

    Create inser_eq.php

---
+++ inser_eq.php
@@ -0,0 +1,19 @@
+<?php
+include 'condb.php';
+
+$eq_name = $_POST['eqname'];
+$eq_id = $_POST['eqid'];
+$eq_b = $_POST['eqb'];
+$eq_mo_name = $_POST['mname'];
+$eq_aname = $_POST['aname'];
+
+$sql="INSERT INTO equipment(name_eq,id_eq,brand_eq,model_eq,amount_eq) VALUES('$eq_name','$eq_id','$eq_b','$eq_mo_name','$eq_aname')";
+$result=mysqli_query($conn,$sql);
+if($result){
+    echo "<script>alert('บันทึกข้อมูลเรียบร้อย');</script>";
+    echo "<script>window.location='show_equipment.php';</script>";
+}else{
+    echo "<script>alert('บันทึกข้อมูลไม่สำเร็จ');</script>";
+}
+mysqli_close($conn);
+?>
