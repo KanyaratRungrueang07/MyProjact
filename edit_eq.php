f6ae2c1b85a6f2fed82b1ec6f917bb90d657f976
KanyaratRungrueang07 <86422790+KanyaratRungrueang07@users.noreply.github.com>
2023-04-26 18:29:55 +0700

    Update edit_eq.php

--- edit_eq.php
+++ edit_eq.php
@@ -8,42 +8,49 @@ $row=mysqli_fetch_array($result);
 
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>แก้ไขข้อมูล</title>
+    <title>form</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-        <div class="row">
-            <div class="col-sm-8">
-                
-    <div class=" h4 text-center alert alert-success mb-4 mt-4" role="alert"> แก้ไขข้อมูล </div>
-    <form method="POST" action="inser_eq.php">
-        <labe>ชื่ออุปกรณ์:</labe>
-        <input type="text" name="eqname_ed" class="form-control" value=<?=$row['id_eq']?> > 
+    <div class="row">
+        <div class="col-sm-8">
+        <div class=" h4 text-center alert alert-success mb-4 mt-4" role="alert"> แก้ไขข้อมูลอุปกรณ์ </div>
+    <form method="POST" action="update_eq.php">
 
-        <labe>รหัสอุปกรณ์:</labe>
-        <input type="number" name="eqid" class="form-control" value=<?=$row['name_eq']?> > 
+    
+    <label>รหัสอุปกรณ์ :</label>
+    <input type="number" name="eqid" class="form-control" readonly value=<?=$row['id_eq']?> > <br>
 
-        <labe>ยี่ห้อ:</labe>
-        <input type="text" name="eqb" class="form-control" value=<?=$row['brand_eq']?> > 
+    <label>ชื่ออุปกรณ์ :</label>
+    <input type="text" name="eqname"  class="form-control" value=<?=$row['name_eq']?> > <br>    
 
-        <labe>รุ่น:</labe>
-        <input type="text" name="mname" class="form-control" value=<?=$row['model_eq']?> >
+    <label>ยี่ห้ออุปกรณ์ :</label>
+    <input type="text" name="eqb" class="form-control" value=<?=$row['brand_eq']?> > <br>
 
-        <labe>จำนวนอุปกรณ์:</labe>
-        <input type="text" name="aname" class="form-control" value=<?=$row['amount_eq']?> > 
+    <label>รุ่นอุปกรณ์ :</label>
+    <input type="text" name="mname" class="form-control" value=<?=$row['model_eq']?>  > <br>
+    
+    <label>จำนวนอุปกรณ์ :</label>
+    <input type="number" name="aname" class="form-control" value=<?=$row['amount_eq']?> > <br>
+    
+    <input type="submit" value="เพิ่ม" class="btn btn-success" >
+    <a href="show_equipment.php" class="btn btn-danger">ยกเลิก</a></a>
 
-        <input type="submit" value="submit" class="btn btn-success">
-        <a href="show_equipment.php" class="btn btn-danger mb-3"> ยกเลิก </a>
-    </form>
-    </div>
-        </div>
-    </div>
+
+
+
+</form>
+</div>
+</div>
+</div>
+</div>
 </body>
 </html>
